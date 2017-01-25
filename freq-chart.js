ff7585920426beb5af3f9af8658200b1b4d50d23
Ahmad K <lance@codemosey.net>
2017-01-24 23:18:10 -0800

    integrates google sheets api

--- freq-chart.js
+++ freq-chart.js
@@ -42,7 +42,7 @@ function assignNormalizedValues(array) {
 function createGraph(data) {
   const {
     CANVAS_HEIGHT, CANVAS_WIDTH, CELL_HEIGHT, CELL_WIDTH, Y_OFFSET,
-  } = getConfig(250);
+  } = getConfig(100);
   const aYearAgo = moment(data[data.length - 1].date)
     .subtract(1, 'Y')
     .day(0)
@@ -56,7 +56,7 @@ function createGraph(data) {
 
   d3.select('svg')
     .remove();
-  const svg = d3.select('div')
+  const svg = d3.select('#chartSpace')
     .append('svg');
   svg.attr('height', CANVAS_HEIGHT + Y_OFFSET)
     .attr('width', CANVAS_WIDTH);
@@ -90,26 +90,125 @@ function createGraph(data) {
     .attr('data-date', (d) => d.date.format())
     .attr('width', CELL_WIDTH)
     .attr('height', CELL_HEIGHT)
-    .style('fill', d => `rgba(0, 200, 100, ${normalizer(d.value)})`);
+    .style('fill', d => `rgba(255, 0, 0, ${normalizer(d.value)})`);
 }
 
-const data = [{
-  date: moment("2016-01-22T19:51:16-08:00"),
-  value: 0,
-}, ];
+// const data = [{
+//   date: moment("2016-01-22T19:51:16-08:00"),
+//   value: 0,
+// }, ];
+//
+// for (var i = 1; i < 366; i++) {
+//   currentDay = moment(data[0].date)
+//     .add(i, 'd');
+//   if (Math.random() * 10 >= 5) {
+//
+//     data.push({
+//       date: currentDay,
+//       value: Math.floor(Math.random() * 9),
+//     });
+//   }
+// }
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   createGraph(data);
+// });
 
-for (var i = 1; i < 366; i++) {
-  currentDay = moment(data[0].date)
-    .add(i, 'd');
-  if (Math.random() * 10 >= 5) {
+/// *** GOOGLE CHARTS STUFF *** ///
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '1068895437155-j6ocpdfo1u3nek6q3svs90nu7mcagnbf.apps.googleusercontent.com';
 
-    data.push({
-      date: currentDay,
-      value: Math.floor(Math.random() * 9),
-    });
+var SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  var authorizeDiv = document.getElementById('authorize-div');
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    authorizeDiv.style.display = 'none';
+    loadSheetsApi();
+  } else {
+    // Show auth UI, allowing the user to initiate authorization by
+    // clicking authorize button.
+    authorizeDiv.style.display = 'inline';
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  createGraph(data);
-});
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
+
+/**
+ * Load Sheets API client library.
+ */
+function loadSheetsApi() {
+  var discoveryUrl =
+      'https://sheets.googleapis.com/$discovery/rest?version=v4';
+  gapi.client.load(discoveryUrl).then(fetchData);
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function fetchData() {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1gol0YokIuIslj-j8YFdpJb-9N9uSRl3M_-1Q1yIdmDY',
+    range: 'Sheet1!A2:B',
+  }).then(function(response) {
+    let data = [];
+    var range = response.result;
+    if (range.values.length > 0) {
+      for (i = 0; i < range.values.length; i++) {
+        var row = range.values[i];
+          data.push({
+            date: moment(row[0]),
+            value: row[1],
+          });
+      }
+      console.log(data);
+      createGraph(data);
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('output');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
